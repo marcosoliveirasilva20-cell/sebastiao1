4bf0977fad83f23f9821f150eafb6152a8ced052
marcosoliveirasilva20-cell <marcos.oliveira.silva20@escola.pr.gov.br>
2026-05-18 09:25:28 -0300

    atualiçao 18 05

--- man.js
+++ man.js
@@ -1,3 +1,8 @@
 const botoes = document.querySelectorAll(".botoes");
-for(let i=0 i<botoes.length;i++){
+for(let i=0; i<botoes.length;i++){
+botoes[i].classList.add("ativo");
+for(let j=j; j < botoes.length; j++){
+    botoes[j].classList.remove("ativo")
 }
+}
+console.log();
